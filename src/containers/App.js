69bcb1cb97266750ec2a9d4c7950aa286554c149
Dustin Walker <dustinwalkerart@gmail.com>
2020-05-17 05:10:53 +0900

    Adds gamepad handler

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import Gamepad from "react-gamepad";
 
-function App() {
-  return (
-    <div className="App">
-      <span>Everything will go here.</span>
-    </div>
-  );
+class App extends Component {
+  connectHandler(padindex) {
+    console.log(`Gamepad ${padindex} connected !`);
+  }
+  disconnectHandler(gamepadIndex) {
+    console.log(`Gamepad ${gamepadIndex} disconnected !`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Gamepad
+          onConnect={this.connectHandler}
+          onDisconnect={this.disconnectHandler}
+          onButtonChange={this.buttonChangeHandler}
+        >
+          <></>
+        </Gamepad>
+        <span>Everything will go here.</span>
+      </div>
+    );
+  }
 }
 
 export default App;
