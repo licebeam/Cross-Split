25e1e2a516d4e88893ff42bc8f145b27cd35f281
Dustin Walker <dustinwalkerart@gmail.com>
2020-05-17 05:12:28 +0900

    Updates

---
+++ precache-manifest.685b224fe26f60618f64e9a18ae6b5f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06651d473119cac1cf11b2e523b49c8",
+    "url": "/Cross-Split/index.html"
+  },
+  {
+    "revision": "2aff1b7c44a11226e849",
+    "url": "/Cross-Split/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2d85bef8c151bc266a44",
+    "url": "/Cross-Split/static/js/2.f8cc3cba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cross-Split/static/js/2.f8cc3cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aff1b7c44a11226e849",
+    "url": "/Cross-Split/static/js/main.695801a3.chunk.js"
+  },
+  {
+    "revision": "15218538a3d7f5f353bf",
+    "url": "/Cross-Split/static/js/runtime-main.0deb78d3.js"
+  }
+]);
